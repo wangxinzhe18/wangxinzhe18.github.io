6fbe934582bf6d09c14bd883a74337b9db88cc77
wangxinzhe18 <2484227417@qq.com>
2023-04-12 13:25:52 +0800

    Site updated: 2023-04-12 13:25:51

---
+++ js/reward.js
@@ -0,0 +1,50 @@
+function reward(){
+    Swal.fire({
+      title: '<strong>您正在为 <u>王同学</u> 充电</strong>',
+      html: '<b>请选择您的付款方式</b>',
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonText:
+        '<i class="iconfont icat-alipay"></i> 支付宝',
+      cancelButtonText:
+        '<i class="iconfont icat-weixin"></i> 微信支付',
+      confirmButtonColor: '#1677FF',
+      cancelButtonColor: '#2AAE67',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: '感谢您',
+          html: '请打开支付宝 <b>[扫一扫]</b> 以充电',
+          imageUrl: '/img/zfb.jpg',
+          imageWidth: 175,
+          imageHeight: 175,
+          imageAlt: 'Custom image'
+        }).then((result) => {
+          Swal.fire(
+            '充电成功qwq',
+            '感谢您对王同学的支持 salute！',
+            'success'
+          )
+        })
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire({
+          title: '感谢您',
+          html: '请打开微信 <b>[扫一扫]</b> 以充电',
+          imageUrl: '/img/wechat.png',
+          imageWidth: 175,
+          imageHeight: 175,
+          imageAlt: 'Custom image'
+        }).then((result) => {
+          Swal.fire(
+            '充电成功',
+            '感谢您的支持',
+            'success'
+          )
+        })
+      }
+    })
+  }
+
+
