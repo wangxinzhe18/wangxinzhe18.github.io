06648e5a55bddc3423050912adf1df739829db03
wangxinzhe18 <2484227417@qq.com>
2023-04-19 10:10:04 +0800

    Site updated: 2023-04-19 10:10:03

---
+++ js/wxzcustom.js
@@ -0,0 +1,3 @@
+"" === GLOBAL_CONFIG_SITE.title.replace("王同学的Blog", "") ? document.getElementById("page-name-text").style.display = "none" : document.querySelector("#page-name-text>span").innerHTML = document.title.split(" | Jayhrn")[0];
+
+
